d3baa41218a8ef4b2e45154c79c7c69556a7f9b8
Anis Ahmad <anisahmad1770@gmail.com>
2023-07-22 15:27:09 +0500

    commit3

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,59 @@ import Home from "./pages/home/home"
 import Products from "./pages/Products/Products"
 import Users from "./pages/users/Users"
 import Errorpage from "./pages/ErrorPage/Errorpage";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
-} from "react-router-dom";
+import {createBrowserRouter,RouterProvider,Route,Outlet,} from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Menu from "./components/menu/Menu";
+import { Children } from "react";
+import Login from "./components/login/Login";
 
 
 function App () {
+  const Layout =() =>{
+    return(
+      <div className="main">
+        <Navbar/>
+        <div className="container">
+          <div className="menu-container">
+            <Menu/>
+          </div>
+          <div className="content-container">
+            <Outlet/>
+          </div>
+        </div>
+        <Footer/>
+      </div>
+    )
+  }
+
   const router = createBrowserRouter([
     {
-      path: "/",
-      element:  <Home/>,
-      
+      path:"/",
+      element:<Layout/>,
+      children: [
+        {
+          path: "/",
+          element:  <Home/>,
+          
+        },
+        {
+          path: "users",
+          element: <Users/>,
+        },
+        {
+          path: "products",
+          element: <Products/>,
+        },
+        {
+          path: "*",
+          element: <Errorpage/>,
+        },
+      ]
     },
     {
-      path: "users",
-      element: <Users/>,
-    },
-    {
-      path: "products",
-      element: <Products/>,
-    },
-    {
-      path: "*",
-      element: <Errorpage/>,
+      path:"login",
+      element: <Login/>
     },
   ]);
   return (
---
+++ src/components/login/Login.tsx
@@ -0,0 +1,11 @@
+
+
+const Login = () => {
+  return (
+    <div>
+      Login Form here
+    </div>
+  )
+}
+
+export default Login
