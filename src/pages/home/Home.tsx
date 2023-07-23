14e36910bd066b95301b3cdbc91feb64d8b49395
Anis Ahmad <anisahmad1770@gmail.com>
2023-07-23 13:25:34 +0500

    commit6

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,7 @@
 
 import ChartBox from "../../components/ChartBox/ChartBox"
 import TopBox from "../../components/topBox/TopBox"
+import { chartBoxUser,chartBoxConversion,chartBoxProduct,chartBoxRevenue } from "../../data"
 import "./home.scss"
 const Home = () => {
   return (
@@ -10,11 +11,11 @@ const Home = () => {
         <TopBox/>
        </div>
 
-       <div className="box box2"><ChartBox/> </div>
-       <div className="box box3"><ChartBox/></div>
+       <div className="box box2"><ChartBox {...chartBoxUser}/> </div>
+       <div className="box box3"><ChartBox{...chartBoxProduct}/></div>
        <div className="box box4">Box4</div>
-       <div className="box box5"><ChartBox/></div>
-       <div className="box box6"><ChartBox/></div>
+       <div className="box box5"><ChartBox{...chartBoxRevenue}/></div>
+       <div className="box box6"><ChartBox{...chartBoxConversion}/></div>
        <div className="box box7">Box7</div>
        <div className="box box8">Box8</div>
        <div className="box box9">Box9</div>
